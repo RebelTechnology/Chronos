d11060792f90f5503348b00d3976c5c08724fbb1
mars <martin@rebel-it.com>
2016-04-08 13:12:24 +0100

    formatting

--- firmware/Source/device.h
+++ firmware/Source/device.h
@@ -1,7 +1,7 @@
 #include <inttypes.h>
 
 #define FIRMWARE_VERSION             "001"
-#define HARDWARE_VERSION             "TapTempo Rev02"
+#define HARDWARE_VERSION             "TapTempo Rev03"
 
 /*
   left toggle PB8 / PB9
@@ -12,37 +12,37 @@
   DAC2 PA5
  */
 
-#define TRIGGER_OUTPUT_PORT       GPIOB
-#define TRIGGER_OUTPUT_PIN        GPIO_Pin_0
+#define TRIGGER_OUTPUT_PORT          GPIOB
+#define TRIGGER_OUTPUT_PIN           GPIO_Pin_0
 
-#define TOGGLE_L_PORT             GPIOB
-#define TOGGLE_L_PIN_A            GPIO_Pin_8
-#define TOGGLE_L_PIN_B            GPIO_Pin_9
-#define TOGGLE_L_CLOCK            RCC_AHB1Periph_GPIOB
+#define TOGGLE_L_PORT                GPIOB
+#define TOGGLE_L_PIN_A               GPIO_Pin_8
+#define TOGGLE_L_PIN_B               GPIO_Pin_9
+#define TOGGLE_L_CLOCK               RCC_AHB1Periph_GPIOB
 
-#define TOGGLE_R_PORT             GPIOA
-#define TOGGLE_R_PIN_A            GPIO_Pin_9
-#define TOGGLE_R_PIN_B            GPIO_Pin_10
-#define TOGGLE_R_CLOCK            RCC_AHB1Periph_GPIOA
+#define TOGGLE_R_PORT                GPIOA
+#define TOGGLE_R_PIN_A               GPIO_Pin_9
+#define TOGGLE_R_PIN_B               GPIO_Pin_10
+#define TOGGLE_R_CLOCK               RCC_AHB1Periph_GPIOA
 
-#define TRIGGER_INPUT_PORT        GPIOB
-#define TRIGGER_INPUT_PIN         GPIO_Pin_1
-#define TRIGGER_INPUT_CLOCK            RCC_APB2Periph_AFIO
-#define TRIGGER_INPUT_PORT_SOURCE      GPIO_PortSourceGPIOB
-#define TRIGGER_INPUT_PIN_SOURCE       GPIO_PinSource1
-#define TRIGGER_INPUT_PIN_LINE         EXTI_Line1
-#define TRIGGER_INPUT_IRQ              EXTI1_IRQn
-#define TRIGGER_INPUT_HANDLER          EXTI1_IRQHandler
+#define TRIGGER_INPUT_PORT           GPIOB
+#define TRIGGER_INPUT_PIN            GPIO_Pin_1
+#define TRIGGER_INPUT_CLOCK          RCC_APB2Periph_AFIO
+#define TRIGGER_INPUT_PORT_SOURCE    GPIO_PortSourceGPIOB
+#define TRIGGER_INPUT_PIN_SOURCE     GPIO_PinSource1
+#define TRIGGER_INPUT_PIN_LINE       EXTI_Line1
+#define TRIGGER_INPUT_IRQ            EXTI1_IRQn
+#define TRIGGER_INPUT_HANDLER        EXTI1_IRQHandler
 
 /* Illuminated pushbutton */
-#define PUSHBUTTON_PORT                GPIOA
-#define PUSHBUTTON_CLOCK               RCC_APB2Periph_AFIO
-#define PUSHBUTTON_PORT_SOURCE         GPIO_PortSourceGPIOA
-#define PUSHBUTTON_PIN                 GPIO_Pin_2
-#define PUSHBUTTON_PIN_SOURCE          GPIO_PinSource2
-#define PUSHBUTTON_PIN_LINE            EXTI_Line2
-#define PUSHBUTTON_IRQ                 EXTI2_IRQn
-#define PUSHBUTTON_HANDLER             EXTI2_IRQHandler
+#define PUSHBUTTON_PORT              GPIOA
+#define PUSHBUTTON_CLOCK             RCC_APB2Periph_AFIO
+#define PUSHBUTTON_PORT_SOURCE       GPIO_PortSourceGPIOA
+#define PUSHBUTTON_PIN               GPIO_Pin_2
+#define PUSHBUTTON_PIN_SOURCE        GPIO_PinSource2
+#define PUSHBUTTON_PIN_LINE          EXTI_Line2
+#define PUSHBUTTON_IRQ               EXTI2_IRQn
+#define PUSHBUTTON_HANDLER           EXTI2_IRQHandler
 
 /* LED */
 #define LED_PORT                     GPIOA
